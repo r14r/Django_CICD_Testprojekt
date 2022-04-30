aee639d02b4937467694278e3c0e36601474fc88
Ralph Göstenmeier <r14r@via-internet.de>
2022-04-30 17:54:23 +0200

    reformatting

--- DjangoApp/polls/models.py
+++ DjangoApp/polls/models.py
@@ -12,8 +12,9 @@ class Question(models.Model):
     Question:
 
     """
+
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
         """
@@ -22,10 +23,10 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.admin_order_field = "pub_date"
     was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
-    
+    was_published_recently.short_description = "Published recently?"
+
     def __str__(self):
         """
         __str__(self):
@@ -38,6 +39,7 @@ class Choice(models.Model):
     Choice:
 
     """
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
